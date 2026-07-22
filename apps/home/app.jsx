54bee62495d6f883afa549adab2b38a2cd0b4257
Travis <twag117@gmail.com>
2026-07-21 21:50:55 -0500

    added to home screen with style

--- apps/home/app.jsx
+++ apps/home/app.jsx
@@ -9,10 +9,22 @@ homeApp.get('/', (c) => {
 
   return c.html(
     <Layout title="Sencho" user={user}>
+      <style>
+        {`
+          .container {
+            display: flex;
+            align-items: start;
+            flex-flow: column;
+            gap: 1rem;
+          }
+        `}
+      </style>
       <h1>Senchō</h1>
       <p>A small collection of games/apps.</p>
-      <a href="/fibfinder" role="button">Fib Finder</a>
-      <a href="/qr" role="button">QR Code Generator</a>
+      <div class="container">
+        <a href="/fibfinder" role="button">Fib Finder</a>
+        <a href="/qr" role="button">QR Code Generator</a>
+      </div>
     </Layout>
   )
 })
