f287e89a91e5140ba3af2fc46bf5c73ec44c618b
Travis <twag117@gmail.com>
2026-08-26 17:26:08 -0500

    fixed qr code issue

--- apps/qrcodegenerator.tsx
+++ apps/qrcodegenerator.tsx
@@ -8,15 +8,13 @@ qrCodeGeneratorApp.get('/', async (c) => {
   const url = c.req.query("url")
 
   if (url) {
-    const generateQR = await QRCode.toDataURL(url)
+    const svg = await QRCode.toString(url, { type: 'svg' })
 
     return c.html(
       <Layout title="QR Code Generator">
         <h1>QR Code Generator</h1>
         <p>Free QR Code Generator</p>
-        <div>
-          <img src={generateQR} alt={url} />
-        </div>
+        <div dangerouslySetInnerHTML={{ __html: svg }} />
         <form method="get" action="/qr">
           <input type="text" name="url" value={url} placeholder="URL" />
           <button type="submit">Generate!</button>
